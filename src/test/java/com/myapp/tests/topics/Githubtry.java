e59aff5f827da31c361c6f4e2b6738c23cdd3f40
mustafa2357 <ozkanahmet075@gmail.com>
2023-06-19 11:36:31 +0200

    v

--- src/test/java/com/myapp/tests/topics/Githubtry.java
+++ src/test/java/com/myapp/tests/topics/Githubtry.java
@@ -3,5 +3,6 @@ package com.myapp.tests.topics;
 public class Githubtry {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("hello");
     }
 }
